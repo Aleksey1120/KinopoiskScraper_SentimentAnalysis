e2033f6ff55a94c6b9694db099cd35c396a5983a
Aleksey1120 <alex.sad322@gmail.com>
2024-03-25 20:30:28 +0300

    test: add classifier app tests

---
+++ tests/test_classifier_api.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+import requests
+import json
+
+from unittest import TestCase
+
+
+class TestClassifierAPI(TestCase):
+    def test_api(self):
+        eps = 0.001
+
+        reviews = ['Очень хороший фильм. Рекомендую.',
+                   'Захватывающее начало, но скомканный финал.',
+                   'Скучный сюжет, шаблонные персонажи, предсказуемый финал. Не тратьте время.']
+
+        response = requests.post('http://localhost:8080/predict', json.dumps({'texts': reviews})).json()
+        self.assertTrue('sentiments' in response)
+        self.assertTrue('confidences' in response)
+        self.assertTrue('probabilities' in response)
+
+        self.assertTrue(response['sentiments'] == ['positive', 'neutral', 'negative'])
+        self.assertTrue(len(response['confidences']) == 3)
+        self.assertTrue(all([pred > 0.33 for pred in response['confidences']]))
+
+        probabilities = pd.DataFrame(response['probabilities'])
+        self.assertTrue(probabilities.shape == (3, 3))
+        self.assertTrue(set(probabilities.columns) == {'negative', 'neutral', 'positive'})
+        self.assertTrue(np.all(np.abs(np.sum(probabilities, axis=1) - 1.0) < eps))
+
+        self.assertTrue(np.all(np.abs(probabilities.max(axis=1) - response['confidences']) < eps))
---
+++ tests/test_model.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import numpy as np
+
+import sys
+
+sys.path.append('app/classifier_app')
+
+from app.classifier_app.model import Model
+from unittest import TestCase
+
+
+class TestModel(TestCase):
+    def test_sentiment(self):
+        eps = 0.001
+        model = Model()
+
+        reviews = ['Очень хороший фильм. Рекомендую.',
+                   'Захватывающее начало, но скомканный финал.',
+                   'Скучный сюжет, шаблонные персонажи, предсказуемый финал. Не тратьте время.']
+
+        predict = model.predict(reviews)
+        self.assertTrue('sentiments' in predict)
+        self.assertTrue('confidences' in predict)
+        self.assertTrue('probabilities' in predict)
+
+        self.assertTrue(predict['sentiments'] == ['positive', 'neutral', 'negative'])
+        self.assertTrue(len(predict['confidences']) == 3)
+        self.assertTrue(all([pred > 0.33 for pred in predict['confidences']]))
+
+        probabilities = pd.DataFrame(predict['probabilities'])
+        self.assertTrue(probabilities.shape == (3, 3))
+        self.assertTrue(set(probabilities.columns) == {'negative', 'neutral', 'positive'})
+        self.assertTrue(np.all(np.abs(np.sum(probabilities, axis=1) - 1.0) < eps))
+
+        self.assertTrue(np.all(np.abs(probabilities.max(axis=1) - predict['confidences']) < eps))
